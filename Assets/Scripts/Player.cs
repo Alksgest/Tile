1e1e3102677f17ce7d491b346ba4aecc8c597d8e
Vladyslav <Alksgest@gmail.com>
2018-07-26 10:57:30 +0300

    added climbing

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,62 +1,80 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
 
-    [SerializeField] private float runSpeed = 0.1f;
+    [SerializeField] private float runSpeed = 5f;
+    [SerializeField] private float climbingSpeed = 5f;
     [SerializeField] private float jumpSpeed = 8f;
 
     private Rigidbody2D myRigidbody;
     private Animator myAnimator;
+    private Collider2D myCollider;
+
+    private float gravityScaleAtStart;
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
+        myCollider = GetComponent<Collider2D>();
+
+        gravityScaleAtStart = myRigidbody.gravityScale;
     }
 
     void Update()
     {
         Run();
+        ClimbLadder();
         Jump();
-        // FlipSprite();
+        FlipSprite();
     }
 
     private void Run()
     {
-        
-        if (Input.GetKey(KeyCode.A))
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
+
+        bool playerHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", playerHorizontalSpeed);
+    }
+    private void Jump()
+    {
+        if (myCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
         {
-            myAnimator.SetBool("Running", true);
-            this.transform.position = new Vector3(this.transform.position.x - runSpeed, this.transform.position.y, this.transform.position.z);
-            this.transform.localScale = new Vector3(-1, 1, 1);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+                myRigidbody.velocity += jumpVelocityToAdd;
+            }
         }
-        else if (Input.GetKey(KeyCode.D))
+    }
+    private void ClimbLadder()
+    {
+        if (myCollider.IsTouchingLayers(LayerMask.GetMask("Climbing")))
         {
-            myAnimator.SetBool("Running", true);
-            this.transform.position = new Vector3(this.transform.position.x + runSpeed, this.transform.position.y, this.transform.position.z);
-            this.transform.localScale = new Vector3(1, 1, 1);
+            myRigidbody.gravityScale = 0f;
+            float controlThrow = CrossPlatformInputManager.GetAxis("Vertical");
+            Vector2 climbVelocity = new Vector2(myRigidbody.velocity.x, controlThrow * climbingSpeed);
+            myRigidbody.velocity = climbVelocity;
+            bool playerVerticalSpeed = Mathf.Abs(myRigidbody.velocity.y) > Mathf.Epsilon;
+            myAnimator.SetBool("Climbing", playerVerticalSpeed);
         }
         else
         {
-            myAnimator.SetBool("Running", false);
+            myAnimator.SetBool("Climbing", false);
+            myRigidbody.gravityScale = gravityScaleAtStart;
         }
     }
-    private void Jump() {
-        if(Input.GetKeyDown(KeyCode.Space))
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
         {
-            //this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + jumpSpeed, this.transform.position.z);
-            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
-            myRigidbody.velocity += jumpVelocityToAdd;
+            this.transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
         }
     }
-    //private void FlipSprite()
-    //{
-    //    bool playerHasHorizontalSpeed = Mathf.Abs(rigidbody2D.velocity.x) > Mathf.Epsilon;
-    //    if(playerHasHorizontalSpeed)
-    //    {
-    //        this.transform.localScale = new Vector2(Mathf.Sign(rigidbody2D.velocity.x), 1f);
-    //    }
-    //}
 }
